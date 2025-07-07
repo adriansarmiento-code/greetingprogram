ebf51f9f1324f2da63c0ff471fedcf05710c43d6
adriansarmiento-code <ransar39@gmail.com>
2025-07-07 12:00:56 +0800

    Add files via upload

---
+++ myGreetings/callGreet.js
@@ -0,0 +1,8 @@
+/*  Sarmiento, Adrian P.
+    WD-302
+    07/07/2025 
+*/
+const greet = require('./greet'); 
+
+console.log(greet("John")); 
+console.log(greet("Jane")); 
---
+++ myGreetings/greet.js
@@ -0,0 +1,9 @@
+/*  Sarmiento, Adrian P.
+    WD-302
+    07/07/2025 
+*/
+function greet(name) {
+    return "Hello, " + name;
+}
+
+module.exports = greet;
